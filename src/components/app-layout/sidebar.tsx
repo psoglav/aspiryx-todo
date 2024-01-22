26ace34987780e353e0ca0ec290e6bd65afa0295
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-01-22 21:12:57 +0300

    feat: wrap BackToHub button with link

--- src/components/app-layout/sidebar.tsx
+++ src/components/app-layout/sidebar.tsx
@@ -17,14 +17,18 @@ export default function Sidebar() {
 
   return (
     <div className='grid h-full grid-rows-[max-content_1fr_max-content] space-y-4'>
-      <Button className="flex w-full gap-2 py-8 uppercase" variant='ghost'>
-        <Icon icon='ion:arrow-back-outline' />
-        <span className="text-lg font-bold">HUB</span>
-      </Button>
+      <a href="https://aspiryx.space">
+        <Button
+          variant='ghost'
+          className="flex w-full gap-2 py-8 uppercase" 
+        >
+          <Icon icon='ion:arrow-back-outline' />
+          <span className="text-lg font-bold">HUB</span>
+        </Button>
+      </a>
       
       <div className='space-y-4'>
         <Input placeholder="Search" />
-
 
         <div className='flex flex-col gap-2'>
           <Link to='/'>
