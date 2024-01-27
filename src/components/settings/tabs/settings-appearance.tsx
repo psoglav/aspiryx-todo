b02bc9081989cc641a44dbd1803f56c361d7801a
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-01-27 15:54:01 +0300

    feat: add simple theme toggle to settings

--- src/components/settings/tabs/settings-appearance.tsx
+++ src/components/settings/tabs/settings-appearance.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { useTheme } from '@/components/ui/theme-provider'
+
 export function SettingsAppearance() {
+  const { theme, setTheme } = useTheme()
+
   return (
     <div className="">
-      appearance
+      <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Toggle theme</Button>
     </div>
   )
 }
