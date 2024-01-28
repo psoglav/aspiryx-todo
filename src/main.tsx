9f9f0e7110a49bac6bfd1e8bd6cbe08a5ddfbb0e
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-01-29 01:28:07 +0300

    feat: add & initialize @vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { inject } from '@vercel/analytics';
 
 import '@/index.css'
 import App from './App'
 import { store } from '@/store/index.ts'
 import { ThemeProvider } from "@/components/ui/theme-provider"
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
