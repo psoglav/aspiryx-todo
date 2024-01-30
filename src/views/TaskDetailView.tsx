c84ecb2c660fcc29cde8f3f89c0c1ea25062c800
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-01-30 19:50:48 +0300

    feat(TaskDetailView): animate transition

--- src/views/TaskDetailView.tsx
+++ src/views/TaskDetailView.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button"
 import { RootState } from "@/store"
 import { Icon } from "@iconify/react/dist/iconify.js"
 import { setActiveTask } from "@/store/main"
+import { AnimatePresence, motion } from "framer-motion"
 
 export function TaskDetailView() {
   const tasks = useSelector((state: RootState) => state.main.tasks)
@@ -12,16 +13,25 @@ export function TaskDetailView() {
   const dispatch = useDispatch()
 
   return (
-    <div className='flex min-w-60 flex-col gap-4 p-4'>
-      <Button 
-        variant='ghost' 
-        size='icon' 
-        className="size-8 self-end text-foreground/50"
-        onClick={() => dispatch(setActiveTask(null))}
+    <AnimatePresence mode="wait">
+      <motion.div 
+        initial={{ scale: 0.99, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 0.1 }}
+        exit={{ scale: 0.99, opacity: 0 }} 
+        key={taskId}
+        className='flex min-w-60 flex-col gap-4 p-4'
       >
-        <Icon icon="material-symbols:close-small-outline-rounded" className="text-3xl" />
-      </Button>
-      {task ? <TaskItem value={task} editable /> : null}
-    </div>
+        <Button 
+          variant='ghost' 
+          size='icon' 
+          className="size-8 self-end text-foreground/50"
+          onClick={() => dispatch(setActiveTask(null))}
+        >
+          <Icon icon="material-symbols:close-small-outline-rounded" className="text-3xl" />
+        </Button>
+        {task ? <TaskItem value={task} editable /> : null}
+      </motion.div>
+    </AnimatePresence>
   )
 }
