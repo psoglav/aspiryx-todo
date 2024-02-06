b4cceff8352a2498775139beb088014cb8d3d882
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-02-06 14:10:23 +0300

    feat: update gradient styles

--- src/components/list/list-header.tsx
+++ src/components/list/list-header.tsx
@@ -78,6 +78,7 @@ export const ListHeader = () => {
                       <DropdownMenuRadioGroup value={currentList.style} onValueChange={onStyleChange}>
                         <DropdownMenuRadioItem value="">Default</DropdownMenuRadioItem>
                         <DropdownMenuRadioItem value="aura">Aura</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="black-swan">Black Swan</DropdownMenuRadioItem>
                         <DropdownMenuRadioItem value="pearl">Pearl</DropdownMenuRadioItem>
                         <DropdownMenuRadioItem value="swamp">Swamp</DropdownMenuRadioItem>
                       </DropdownMenuRadioGroup>
--- src/components/whatamesh/gradient.constants.ts
+++ src/components/whatamesh/gradient.constants.ts
@@ -6,12 +6,18 @@ export const BgStyles: Record<ListStyle, string[] | Partial<Record<Theme, string
     dark: ['#000000', '#111111', '#4800ff', '#000000'],
     light: ['#ffffff', '#eeeeee', '#4800ff', '#ffffff'],
   },
+  'black-swan': [
+    '#000000',
+    '#111111',
+    '#ffffff',
+    '#000000'
+  ],
   pearl: {
     dark: [
-      '#000000',
-      '#111111',
-      '#ffffff',
-      '#000000'
+      '#7aa9c2',
+      '#2ba2ce',
+      '#173d49',
+      '#0c103a'
     ],
     light: [
       '#c3e4ff',
