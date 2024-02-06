2a82e3c24883c7c51852d609e7ce53d85d86e9e0
Andrey Danilenko <psoglav.ih8u@gmail.com>
2024-02-06 22:57:27 +0300

    feat: replace hub link with profile dropdown

--- src/components/app-layout/sidebar.tsx
+++ src/components/app-layout/sidebar.tsx
@@ -12,6 +12,7 @@ import { Separator } from '@/components/ui/separator'
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { SettingsContext } from '@/components/settings';
 import { PoweredBy } from '@/components/powered-by';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export default function Sidebar() {
   const lists = useSelector((state: RootState) => state.main.lists)
@@ -24,17 +25,30 @@ export default function Sidebar() {
     <div className='app-drag grid h-full grid-rows-[max-content_1fr_max-content] space-y-2 p-4 pr-1'>
       <div className='flex flex-col gap-4 pr-3'>
         <div className="app-no-drag flex gap-2">
-          {!window.isElectronApp && (
+          {!window.isElectronApp ? (
             <a href="https://aspiryx.space" className='grow'>
               <Button
                 variant='ghost'
                 className="flex size-full justify-start gap-2 uppercase" 
               >
                 <Icon icon='ion:arrow-back-outline' />
-                <span className=" font-bold">HUB</span>
+                <span className="font-bold">HUB</span>
               </Button>
             </a>
+          ) : (
+            <Button
+              variant='ghost'
+              className="flex grow justify-start gap-3" 
+            >
+              <Avatar className='size-6'>
+                <AvatarImage src="https://aspiryx.space/logo.svg" />
+                <AvatarFallback>AD</AvatarFallback>
+              </Avatar>
+              <span className="font-bold">Profile</span>
+            </Button>
           )}
+
+          
           <Button variant='ghost' size='icon' onClick={() => setOpen(true)}>
             <Icon icon='material-symbols:settings' className='text-xl' />
           </Button>
